8d9f80d4dae3716f52e9aaf0940da65c7af3d925
Olga Lorenz <ol900ivas@gmail.com>
2023-03-29 16:49:52 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,43 @@ export class App extends Component {
   state = {
     contacts: [],
     name: '',
+    // filter: '',
+    number: '',
+  };
+
+  handleInputChange = event => {
+    console.log(event.currentTarget.value);
+    this.setState({ name: event.currentTarget.value });
   };
 
   render() {
-    return <div>React</div>;
+    return (
+      <div>
+        <form>
+          <label>
+            Name
+            <input
+              type="text"
+              name={this.state.name}
+              onChange={this.handleInputChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+
+          <label>
+            Number
+            <input
+              type="tel"
+              name={this.state.number}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+        </form>
+      </div>
+    );
   }
 }
